dc63c59d68c2db2a68c9b946e461ceb4573ca68b
Heerko <heerko@swummoq.net>
2021-07-17 23:43:23 +0200

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,39 @@
+import { Config } from '@stencil/core';
+import {JsonDocs} from '@stencil/core/internal';
+
+import {sass} from '@stencil/sass';
+import {postcss} from '@stencil/postcss';
+// @ts-ignore
+import autoprefixer from 'autoprefixer';
+
+import {generateDesc} from './deckdeckgo.desc';
+
+export const config: Config = {
+  namespace: 'my-template',
+  outputTargets:[
+    {
+      type: 'dist'
+    },
+    {
+      type: 'www',
+      serviceWorker: null
+    },
+    {
+      type: 'docs-custom',
+      generator: (docs: JsonDocs) => {
+        generateDesc(docs);
+      },
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    }
+  ],
+  plugins: [
+    sass({
+      includePaths: ['node_modules/@deckdeckgo/slide-utils/styles/']
+    }),
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ]
+};
